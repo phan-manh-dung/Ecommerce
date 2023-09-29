ac7ef2f751ad98a69cab14a5bfca5658b91c28b8
dungggg1203 <dungcongnghiep4@gmail.com>
2023-09-30 01:38:18 +0700

    ui_4

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -2,66 +2,64 @@ import React from 'react';
 import styles from './SignIn.module.scss';
 import classNames from 'classnames/bind';
 
-import img_background from '~/assets/img_Global/img_background.jpg';
-import { Input } from 'antd';
+import { Checkbox, Input } from 'antd';
+import ButtonComponent from '~/component/ButtonComponent/Buttoncomponent';
+import { FacebookOutlined, GithubOutlined, HomeOutlined, InstagramOutlined, LinkedinOutlined } from '@ant-design/icons';
 
 const cx = classNames.bind(styles);
 
 const SignInPage = () => {
     return (
         <div className={cx('container_wrapper')}>
-            {/* <img src={img_background} style={{ maxWidth: '100%' }} /> */}
-            <header class="header">
-                <nav class="nav">
-                    <a href="#"> Home </a>
-                    <a href="#"> About </a>
-                    <a href="#"> Service </a>
-                    <a href="#"> Contact </a>
-                </nav>
-                <div class="search">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                    <i class="fa-solid fa-user"></i>
-                </div>
-            </header>
-            <div class="background"></div>
-            <section class="home">
-                <div class="content">
-                    <a href="#" class="logo">
-                        {' '}
-                        <i class="fa-solid fa-paper-plane"></i>Ddoans
-                    </a>
+            <header className={cx('header')}></header>
+            <div className={cx('background')}></div>
+            <section className={cx('home')}>
+                <div className={cx('content')}>
                     <h2> Welcome!</h2>
-                    <h3> To Our Nem Website </h3>
-                    <pre> Lorem ipsum, dolor sit amen consenter adipisicing elit. Beatae,asperiores </pre>
-                    <div class="icon">
-                        <i class="fa-brands fa-instagram"></i>
-                        <i class="fa-brands fa-facebook"></i>
-                        <i class="fa-brands fa-twitter"></i>
-                        <i class="fa-brands fa-github"></i>
+                    <h3> To Our Manh Dung Website </h3>
+                    <pre>Wishing you a pleasant visit to the website</pre>
+                    <div className={cx('icon')}>
+                        <a className={cx('icon_brand')} href="https://www.facebook.com/jas.murad.353">
+                            <FacebookOutlined style={{ width: '20px', height: '20px' }} />
+                        </a>
+
+                        <a href="/" className={cx('icon_brand')}>
+                            <GithubOutlined style={{ width: '20px', height: '20px' }} />
+                        </a>
+
+                        <a href="/" className={cx('icon_brand')}>
+                            <InstagramOutlined style={{ width: '20px', height: '20px' }} />
+                        </a>
+
+                        <a href="/" className={cx('icon_brand')}>
+                            <LinkedinOutlined style={{ width: '20px', height: '20px' }} />
+                        </a>
+                    </div>
+                    <div className={cx('home_back')}>
+                        <a href="/">
+                            <HomeOutlined style={{ color: '#fff', fontSize: '30px' }} />
+                        </a>
                     </div>
                 </div>
-                <div class="login">
+                <div className={cx('login')}>
                     <h2> Login </h2>
-                    <div class="input">
+                    <div className={cx('input')}>
                         <Input placeholder="Nhập email" />
-                        <i class="fa-solid fa-envelope"></i>
                     </div>
-                    <div class="input">
+                    <div className={cx('input')}>
                         <Input placeholder="Nhập password" />
-                        <i class="fa-solid fa-lock"></i>
                     </div>
-                    <div class="check">
+                    <div className={cx('check')}>
                         <label>
-                            {' '}
-                            <Input />
-                            Remember me{' '}
+                            <Checkbox />
+                            <span> Remember me</span>
                         </label>
                         <a href="#"> Forgot Password?</a>
                     </div>
-                    <div class="button">
-                        <button class="btn"> Login </button>
+                    <div className={cx('button')}>
+                        <ButtonComponent className={cx('btn')} textButton={'Login'} backgroundColor="rgb(254,67,79)" />
                     </div>
-                    <div class="sign-up">
+                    <div className={cx('sign-up')}>
                         <p> Don't have an account?</p>
                         <a href="#"> Sign up</a>
                     </div>
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,75 @@
 import React from 'react';
+import styles from './SignUp.module.scss';
+import classNames from 'classnames/bind';
+import logo_shop from '../../assets/img_Global/logoshop.png';
 
-const SignUpPage = () => {
-    return <div>SignUpPage</div>;
+import { Input } from 'antd';
+import ButtonComponent from '~/component/ButtonComponent/Buttoncomponent';
+import { FacebookOutlined, GithubOutlined, HomeOutlined, InstagramOutlined, LinkedinOutlined } from '@ant-design/icons';
+
+const cx = classNames.bind(styles);
+
+const SignInPage = () => {
+    return (
+        <div className={cx('container_wrapper')}>
+            <header className={cx('header')}>
+                {/* <div className={cx('logo_home')}>
+                    <HomeOutlined />
+                </div> */}
+            </header>
+            <div className={cx('background')}></div>
+            <section className={cx('home')}>
+                <div className={cx('content')}>
+                    <div>
+                        <h2> Welcome!</h2>
+                        <h3> To Our Manh Dung Website </h3>
+                        <pre>Wishing you a pleasant visit to the website</pre>
+                    </div>
+                    <div className={cx('icon')}>
+                        <a className={cx('icon_brand')} href="https://www.facebook.com/jas.murad.353">
+                            <FacebookOutlined style={{ width: '20px', height: '20px' }} />
+                        </a>
+
+                        <a href="/" className={cx('icon_brand')}>
+                            <GithubOutlined style={{ width: '20px', height: '20px' }} />
+                        </a>
+
+                        <a href="/" className={cx('icon_brand')}>
+                            <InstagramOutlined style={{ width: '20px', height: '20px' }} />
+                        </a>
+
+                        <a href="/" className={cx('icon_brand')}>
+                            <LinkedinOutlined style={{ width: '20px', height: '20px' }} />
+                        </a>
+                    </div>
+                    <div className={cx('home_back')}>
+                        <a href="/">
+                            <HomeOutlined style={{ color: '#fff', fontSize: '30px' }} />
+                        </a>
+                    </div>
+                </div>
+                <div className={cx('login')}>
+                    <h2> Sign Up </h2>
+                    <div className={cx('input')}>
+                        <Input placeholder="Nhập email" />
+                    </div>
+                    <div className={cx('input')}>
+                        <Input placeholder="Nhập password" />
+                    </div>
+                    <div className={cx('input')}>
+                        <Input placeholder="Nhập lại password" />
+                    </div>
+                    <div className={cx('button')}>
+                        <ButtonComponent className={cx('btn')} textButton={'Login'} backgroundColor="rgb(254,67,79)" />
+                    </div>
+                    <div className={cx('sign-up')}>
+                        <p> Already have an account?</p>
+                        <a href="#"> Sign in</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 };
 
-export default SignUpPage;
+export default SignInPage;
