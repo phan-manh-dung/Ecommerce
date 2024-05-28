7b5be95362e4bc2ecfd14e0d3b85a47b8afbdf8d
phanmanhdung <phanmanhdung2k3@gmail.com>
2024-05-28 22:52:45 +0700

    config_28052024

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,20 @@
-const {
-  override,
- useBabelRc
-} = require("customize-cra");
+const { override, useBabelRc, overrideDevServer } = require('customize-cra');
+// override: Được sử dụng để ghi đè cấu hình Webpack mặc định của Create React App.
+//useBabelRc: Cho phép sử dụng cấu hình Babel từ file .babelrc.
+// overrideDevServer: Được sử dụng để ghi đè cấu hình DevServer mặc định của Create React App.
 
-module.exports = override(
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useBabelRc()
-);
+// hàm tạo cấu hình dev derver
+const devServerConfig = () => (config) => {
+    config.allowedHosts = 'all'; // all cho phép tất cả các host truy cập
+    return config;
+};
+
+module.exports = {
+    webpack: override(
+        // cho phép sử dụng cấu hình Babel từ file .babelrc trong dự án.
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        useBabelRc(),
+    ),
+    // devServerConfig. Điều này thiết lập allowedHosts cho DevServer theo cấu hình đã chỉ định.
+    devServer: overrideDevServer(devServerConfig()),
+};
